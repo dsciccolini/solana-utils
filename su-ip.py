945c0740211be0defa4c4261952f7c5aa5b2eb9b
dscic <dsciccolini@gmail.com>
2025-03-18 20:21:31 -0400

    Update Tue 03/18/2025 20:21:31.43

--- su-ip.py
+++ su-ip.py
@@ -16,15 +16,16 @@ def get_ip_data(url, validator_address):
             if node.get("identityPubkey") == validator_address:
                 ip_address = node.get('ipAddress', 'N/A')
                 details = handler.getDetails(ip_address)
+                details_all = details.all
                 print(f"\n\033[1;36mTHW-Utils v0 | IP Info | Cluster: {url}\033[0m\n")
                 print(f"{'IP Address:':<15} {ip_address}")
-                print(f"{'ASN:':<15} {details.asn}")
-                print(f"{'Hostname:':<15} {details.hostname}")
-                print(f"{'Organization:':<15} {details.org}")
-                print(f"{'City:':<15} {details.city}")
-                print(f"{'Region:':<15} {details.region}")
-                print(f"{'Country:':<15} {details.country}")
-                print(f"{'Location:':<15} {details.loc}")
+                print(f"{'ASN:':<15} {details_all.get('asn', {}).get('asn', 'N/A')}")
+                print(f"{'Hostname:':<15} {details_all.get('hostname', 'N/A')}")
+                print(f"{'Organization:':<15} {details_all.get('company', {}).get('name', 'N/A')}")
+                print(f"{'City:':<15} {details_all.get('city', 'N/A')}")
+                print(f"{'Region:':<15} {details_all.get('region', 'N/A')}")
+                print(f"{'Country:':<15} {details_all.get('country_name', 'N/A')}")
+                print(f"{'Location:':<15} {details_all.get('loc', 'N/A')}")
                 return
 
         print("Validator not found in gossip data.")
@@ -32,6 +33,8 @@ def get_ip_data(url, validator_address):
         print("Error: Failed to fetch IP data.")
     except json.JSONDecodeError:
         print("Error: Unable to parse gossip data.")
+    except AttributeError as e:
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
