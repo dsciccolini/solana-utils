d3e8905036ff9f8f091a98601e3e1aac74c6341f
dscic <dsciccolini@gmail.com>
2025-03-18 20:19:20 -0400

    Update Tue 03/18/2025 20:19:20.67

--- su-ip.py
+++ su-ip.py
@@ -7,7 +7,7 @@ access_token = '118ed2d59487de'
 handler = ipinfo.getHandler(access_token)
 
 def get_ip_data(url, validator_address):
-    """Fetch IP data in JSON format."""
+    """Fetch IP data in JSON format and print IP info."""
     try:
         result = subprocess.check_output(["solana", "gossip", "--url", url, "--output", "json"], text=True)
         gossip_data = json.loads(result)
@@ -18,11 +18,13 @@ def get_ip_data(url, validator_address):
                 details = handler.getDetails(ip_address)
                 print(f"\n\033[1;36mTHW-Utils v0 | IP Info | Cluster: {url}\033[0m\n")
                 print(f"{'IP Address:':<15} {ip_address}")
-                print({details.asn: details.asn})
-                name = details.hostname
-                print({details.asn: details.hostname})
-                print({details.asn: name})
-                print(f"Location: {details.loc}")
+                print(f"{'ASN:':<15} {details.asn}")
+                print(f"{'Hostname:':<15} {details.hostname}")
+                print(f"{'Organization:':<15} {details.org}")
+                print(f"{'City:':<15} {details.city}")
+                print(f"{'Region:':<15} {details.region}")
+                print(f"{'Country:':<15} {details.country}")
+                print(f"{'Location:':<15} {details.loc}")
                 return
 
         print("Validator not found in gossip data.")
